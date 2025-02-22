1840257a6d3a6771568bdee1959689834914ec99
WillCast <williamisrael210@gmail.com>
2025-02-22 11:34:43 -0500

    Configure - Configurated the first step to microservice mailchimp

---
+++ microservice-mailchimp/src/main/java/com/kayzenmicroservices/mailchimp/MicroserviceMailchimpApplication.java
@@ -0,0 +1,13 @@
+package com.kayzenmicroservices.mailchimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceMailchimpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceMailchimpApplication.class, args);
+	}
+
+}
---
+++ microservice-mailchimp/src/test/java/com/kayzenmicroservices/mailchimp/MicroserviceMailchimpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kayzenmicroservices.mailchimp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceMailchimpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
