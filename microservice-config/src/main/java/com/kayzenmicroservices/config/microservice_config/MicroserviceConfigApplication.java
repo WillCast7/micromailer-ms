8acaf0a001facd059e3677087fbd41eb2f14640a
WillCast <williamisrael210@gmail.com>
2025-02-24 17:56:43 -0500

    Add - Added the enviroment variables, and webclient for consume the api

--- microservice-config/src/main/java/com/kayzenmicroservices/config/microservice_config/MicroserviceConfigApplication.java
+++ microservice-config/src/main/java/com/kayzenmicroservices/config/microservice_config/MicroserviceConfigApplication.java
@@ -2,6 +2,7 @@ package com.kayzenmicroservices.config.microservice_config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class MicroserviceConfigApplication {
